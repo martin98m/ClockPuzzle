f97a7b2946031d3391a6090688b7c8c42c1234dd
Martin Mucha <martin.mucha.3@student.tuke.sk>
2020-11-22 18:14:34 +0100

    Hour hand indicator added

--- js/ClockPuzzle.js
+++ js/ClockPuzzle.js
@@ -1,8 +1,6 @@
 console.log("ClockPuzzle.js loading...");
 
 const clockColor = ["red", "yellow", "blue", "green", "purple", "pink"];
-const puzzleSize = 8;
-
 let clockArray = [3, 3, 2, 4, 3, 1, 2, 3];
 
 let game = {
@@ -19,37 +17,38 @@ clockArray.forEach(function (item, index) {
     // el.setAttribute('class', `number number${i}`);
     hourSpot.className = "hourSpot";
     //hourSpot.id = "ID0" + index;
-    let hourSpotItems = document.createElement("div");
-    hourSpotItems.className = "hour_circle";
-    hourSpotItems.innerText = item.toString();
-    hourSpot.appendChild(hourSpotItems);
+    let hourSpotCircle = document.createElement("div");
+    hourSpotCircle.className = "hour_circle";
+    hourSpotCircle.innerText = item.toString();
+    hourSpotCircle.id = "ID0" + index;
+    hourSpot.appendChild(hourSpotCircle);
 
     hourSpot.style.transform = `rotate(${index*(360 / game.hourCount)}deg)`;
 
 
     document.getElementsByClassName("clock_positions")[0].appendChild(hourSpot);
-    game.hourSpots[hourSpot.id] = {
+    game.hourSpots[hourSpotCircle.id.toString()] = {
         "order" : index,
         "locked" : false,
         "clicked" : false,
         "value" : item,
-        "element" : hourSpot
-    }
-// .element = hourSpot;
+        "element" : hourSpotCircle
+    };
 });
-
-// console.log(game.hourSpots);
 
 let leftHandle = document.getElementById("left_hand");
 let rightHandle = document.createElement("right_hand");
-leftHandle.style.visibility = "hidden";
-rightHandle.style.visibility = "hidden";
+//leftHandle.style.visibility = "hidden";
+//rightHandle.style.visibility = "hidden";
+//CREATED ALL COMPONENTS THAT WERE NEEDED
 
-let items = document.getElementsByClassName("hourSpot");
+
+
+let items = document.getElementsByClassName("hour_circle");
 for (let i = 0; i < items.length; i++){
     items[i].addEventListener("click", function (x) {
         console.log(x.target);
-        let hourSpot = game.hourSpots[x.target.id];
+        let hourSpot = game.hourSpots[x.target.id.toString()];
 
         if (hourSpot.clicked || hourSpot.locked) {
             console.log("Already clicked or locked");
@@ -57,7 +56,7 @@ for (let i = 0; i < items.length; i++){
         }
 
         hourSpot.clicked = true;
-        hourSpot.element.style.opacity = "70%";
+        hourSpot.element.style.opacity = "40%";
         game.clicks = game.clicks + 1;
         if (game.clicks === game.hourCount) alert("Game WON!!!");
 
@@ -73,11 +72,16 @@ for (let i = 0; i < items.length; i++){
 
         let spotLeft = game.hourSpots["ID0" + leftHandePosition.toString()]
         let spotRight = game.hourSpots["ID0" + rightHandlePosition.toString()]
-        if (spotLeft.clicked === false) spotLeft.locked = false;
-        if (spotRight.clicked === false) spotRight.locked = false;
-
-        leftHandle.className = "green " + "clock_hand_position_" + leftHandePosition;
-        rightHandle.className = "green " + "clock_hand_position_" + rightHandlePosition;
+        if (spotLeft.clicked === false) {
+            spotLeft.locked = false;
+            spotLeft.element.style.background = 'red';
+        }
+        if (spotRight.clicked === false){
+            spotRight.locked = false;
+            spotRight.element.style.background = 'blue';
+        }
+        if (spotLeft === spotRight)
+            spotRight.element.style.background = 'green';
     })
 }
 
@@ -95,19 +99,6 @@ function lockHourSpots() {
     console.log(game.hourSpots);
     for ( const x in game.hourSpots){
         game.hourSpots[x].locked = true;
+        game.hourSpots[x].element.style.background = 'white';
     }
 }
-/*
-let drawTicks = function() {
-
-    for (let i = 1; i < 13; i++) {
-
-        let el = document.createElement('div');
-        el.setAttribute('class', `number number${i}`);
-        el.style.transform = `rotate(${i*30}deg)`;
-
-        document.querySelector('.clock_positions').appendChild(el);
-
-    }
-
-}; drawTicks()*/
