c2bbd4a4c499c92f04f989da7721b953bb9a44c7
Martin Mucha <martin.mucha.3@student.tuke.sk>
2020-11-24 22:51:24 +0100

    Added puzzle generator.

--- js/ClockPuzzle.js
+++ js/ClockPuzzle.js
@@ -1,134 +1,194 @@
 console.log("ClockPuzzle.js loading...");
 
 const clockColor = ["red", "yellow", "blue", "green", "purple", "pink"];
-let clockArray = [3, 3, 2, 4, 3, 1, 2, 3];
+// let clockArray = [3, 3, 2, 4, 3, 1, 2, 3];
+let clockArray = [];
 
 let game = {
     "hourSpots":{},
-    "hourCount": 8,
+    "hourCount": null,
     "leftHand": -1,
     "rightHand": -1,
     "clicks": 0
 }
 
-clockArray.forEach(function (item, index) {
-    let hourSpot = document.createElement("div");
+function newGame(size){
 
-    hourSpot.className = "hourSpot";
-    let hourSpotCircle = document.createElement("div");
-    hourSpotCircle.className = "hour_circle";
-    hourSpotCircle.innerText = item.toString();
-    hourSpotCircle.id = "ID0" + index;
-    hourSpot.appendChild(hourSpotCircle);
+    for (let a = 0; a < size; a++) clockArray[a]=0;
+    console.log(clockArray);
+    game.hourCount = size;
+    let newTest = Array.from(Array(size).keys());
+    console.log(newTest);
 
-    hourSpot.style.transform = `rotate(${index*(360 / game.hourCount)}deg)`;
+    let start = Math.floor(Math.random() * Math.floor(newTest.length));//INDEX
 
+    console.log(start);
+    console.log("-------------");
 
-    document.getElementById("main_container").appendChild(hourSpot);
-    game.hourSpots[hourSpotCircle.id.toString()] = {
-        "order" : index,
-        "locked" : false,
-        "clicked" : false,
-        "value" : item,
-        "element" : hourSpot
-    };
-});
+    let o;
+    let x = 1;
+    while(x !== size) {
+        o = newTest[start];//VALUE
+
+        let index = newTest.indexOf(o);
+        if (index !== -1) {
+            newTest.splice(index, 1);
+        }
+        console.log(newTest);
+        console.log("A:" + o);
+
+        let target = Math.floor(Math.random() * Math.floor(newTest.length));//INDEX
+        console.log("B:" + newTest[target]);
+
+        let distanceA = distance(o, newTest[target], size);//VALUE
+        console.log(distanceA);
+
+        clockArray[o] = distanceA;
+        start = target;
+
+        console.log(clockArray);
+        console.log("---------");
+        x++;
+        if (x === size) o = newTest[target];
+    }
+
+    clockArray[o] = 1 + Math.floor(Math.random() * Math.floor(size/2));
+    /*
+    for (let x = 0; x < size; x++){
+        let target = Math.floor(Math.random() * Math.floor(newTest.length));
+
+        clockArray[x] = distance(x,target,size);
+    }
+    */
+    //console.log(clockArray);
+
+    gameSetUp();
+}
 
 let leftHandle = document.getElementById("left_hand");
 let rightHandle = document.getElementById("right_hand");
 leftHandle.style.visibility = "hidden";
 rightHandle.style.visibility = "hidden";
+
+
+function gameSetUp() {
+    clockArray.forEach(function (item, index) {
+        let hourSpot = document.createElement("div");
+
+        hourSpot.className = "hourSpot";
+        let hourSpotCircle = document.createElement("div");
+        hourSpotCircle.className = "hour_circle";
+        hourSpotCircle.innerText = item.toString();
+        hourSpotCircle.id = "ID0" + index;
+        hourSpot.appendChild(hourSpotCircle);
+
+        hourSpot.style.transform = `rotate(${index * (360 / game.hourCount)}deg)`;
+
+
+        document.getElementById("main_container").appendChild(hourSpot);
+        game.hourSpots[hourSpotCircle.id.toString()] = {
+            "order": index,
+            "locked": false,
+            "clicked": false,
+            "value": item,
+            "element": hourSpot
+        };
+    });
+
+    let items = document.getElementsByClassName("hour_circle");
+    for (let i = 0; i < items.length; i++){
+        items[i].addEventListener("click", function (x) {
+            let hourSpot = game.hourSpots[x.target.id.toString()];
+
+            if (hourSpot.clicked || hourSpot.locked) {
+                console.log("Already clicked or locked");
+                return;
+            }
+
+            hourSpot.clicked = true;
+            hourSpot.element.style.opacity = "40%";
+            game.clicks = game.clicks + 1;
+            if (game.clicks === game.hourCount) alert("Game WON!!!");
+
+
+            lockHourSpots();
+
+            let num = parseInt(hourSpot.order);
+
+            leftHandle.style.transform = hourSpot.element.style.transform;
+            rightHandle.style.transform = hourSpot.element.style.transform;
+
+            leftHandle.style.visibility = "visible";
+            rightHandle.style.visibility = "visible";
+
+            let leftHandlePosition = moveLeft(num, hourSpot.value, game.hourCount);
+            let rightHandlePosition = moveRight(num, hourSpot.value, game.hourCount);
+
+            // console.log("NUM",num, " |newLeft:", leftHandlePosition," |newRight:", rightHandlePosition);
+            let resultLeft;
+
+            if (num > leftHandlePosition){
+                //5 > 3 --> go counter clockwise
+                resultLeft = `rotate(${leftHandlePosition*(360 / game.hourCount)}deg)`;
+            }else {
+                //1 < 7  ---> go counterclockwise
+                resultLeft = `rotate(${-360 + leftHandlePosition*(360 / game.hourCount)}deg)`;
+            }
+
+            // console.log(leftHandle.style.transform);
+            // console.log(resultLeft);
+
+            leftHandle.animate([
+                { transform: leftHandle.style.transform },
+                { transform: resultLeft }
+            ], {
+                // timing options
+                duration: 1000
+            });
+            leftHandle.style.transform = resultLeft;
+
+            let resultRight;
+            if (num < rightHandlePosition){
+                resultRight = `rotate(${rightHandlePosition*(360 / game.hourCount)}deg)`;
+            }else {
+                resultRight = `rotate(${360 + rightHandlePosition*(360 / game.hourCount)}deg)`;
+            }
+
+            // console.log(rightHandle.style.transform);
+            // console.log(resultRight);
+
+            rightHandle.animate([
+                { transform: rightHandle.style.transform },
+                { transform: resultRight }
+            ], {
+                // timing options
+                duration: 1000
+            });
+            rightHandle.style.transform = resultRight;
+
+
+            let spotLeft = game.hourSpots["ID0" + leftHandlePosition.toString()]
+            let spotRight = game.hourSpots["ID0" + rightHandlePosition.toString()]
+            if (spotLeft.clicked === false) {
+                spotLeft.locked = false;
+                spotLeft.element.style.background = 'red';
+            }
+            if (spotRight.clicked === false){
+                spotRight.locked = false;
+                spotRight.element.style.background = 'blue';
+                rightHandle.style.transform = `rotate(${rightHandlePosition*(360 / game.hourCount)}deg)`
+            }
+            if (spotLeft === spotRight)
+                spotRight.element.style.background = 'green';
+        })
+    }
+}
+
 //CREATED ALL COMPONENTS THAT WERE NEEDED
 
 
 
-let items = document.getElementsByClassName("hour_circle");
-for (let i = 0; i < items.length; i++){
-    items[i].addEventListener("click", function (x) {
-        let hourSpot = game.hourSpots[x.target.id.toString()];
 
-        if (hourSpot.clicked || hourSpot.locked) {
-            console.log("Already clicked or locked");
-            return;
-        }
-
-        hourSpot.clicked = true;
-        hourSpot.element.style.opacity = "40%";
-        game.clicks = game.clicks + 1;
-        if (game.clicks === game.hourCount) alert("Game WON!!!");
-
-
-        lockHourSpots();
-
-        let num = parseInt(hourSpot.order);
-
-        leftHandle.style.transform = hourSpot.element.style.transform;
-        rightHandle.style.transform = hourSpot.element.style.transform;
-
-        leftHandle.style.visibility = "visible";
-        rightHandle.style.visibility = "visible";
-
-        let leftHandlePosition = moveLeft(num, hourSpot.value, game.hourCount);
-        let rightHandlePosition = moveRight(num, hourSpot.value, game.hourCount);
-
-        // console.log("NUM",num, " |newLeft:", leftHandlePosition," |newRight:", rightHandlePosition);
-        let resultLeft;
-
-        if (num > leftHandlePosition){
-            //5 > 3 --> go counter clockwise
-            resultLeft = `rotate(${leftHandlePosition*(360 / game.hourCount)}deg)`;
-        }else {
-            //1 < 7  ---> go counterclockwise
-            resultLeft = `rotate(${-360 + leftHandlePosition*(360 / game.hourCount)}deg)`;
-        }
-
-        // console.log(leftHandle.style.transform);
-        // console.log(resultLeft);
-
-        leftHandle.animate([
-            { transform: leftHandle.style.transform },
-            { transform: resultLeft }
-        ], {
-            // timing options
-            duration: 1000
-        });
-        leftHandle.style.transform = resultLeft;
-
-        let resultRight;
-        if (num < rightHandlePosition){
-            resultRight = `rotate(${rightHandlePosition*(360 / game.hourCount)}deg)`;
-        }else {
-            resultRight = `rotate(${360 + rightHandlePosition*(360 / game.hourCount)}deg)`;
-        }
-
-        // console.log(rightHandle.style.transform);
-        // console.log(resultRight);
-
-        rightHandle.animate([
-            { transform: rightHandle.style.transform },
-            { transform: resultRight }
-        ], {
-            // timing options
-            duration: 1000
-        });
-        rightHandle.style.transform = resultRight;
-
-
-        let spotLeft = game.hourSpots["ID0" + leftHandlePosition.toString()]
-        let spotRight = game.hourSpots["ID0" + rightHandlePosition.toString()]
-        if (spotLeft.clicked === false) {
-            spotLeft.locked = false;
-            spotLeft.element.style.background = 'red';
-        }
-        if (spotRight.clicked === false){
-            spotRight.locked = false;
-            spotRight.element.style.background = 'blue';
-            rightHandle.style.transform = `rotate(${rightHandlePosition*(360 / game.hourCount)}deg)`
-        }
-        if (spotLeft === spotRight)
-            spotRight.element.style.background = 'green';
-    })
-}
 
 function moveLeft(start, value, maxSize){
     let result = start - value;
@@ -138,6 +198,13 @@ function moveLeft(start, value, maxSize){
 function moveRight(start, value, maxSize){
     let result = start + value;
     return result < maxSize ? result : result % maxSize;
+}
+function distance(start, finish, maxSize) {
+
+    let oneWay = start > finish ? start - finish : finish - start;
+    let otherWay = maxSize - oneWay;
+
+    return oneWay <= otherWay ? oneWay : otherWay;
 }
 
 function lockHourSpots() {
@@ -150,20 +217,23 @@ function lockHourSpots() {
 
 //DFS PUZZLE SOLVER
 
-
 let graph = {};
-clockArray.forEach(function (item, index) {
-    // console.log( item, index);
-   let left = moveLeft(index,item,game.hourCount);
-   let right = moveRight(index, item, game.hourCount);
-   if (left === right){
-       graph[index] = [left];
-   } else {
-       graph[index] = [left, right];
-   }
-});
-//console.log("GRAPH");
-//console.log(graph);
+
+function dfsSetUp() {
+
+    clockArray.forEach(function (item, index) {
+        // console.log( item, index);
+        let left = moveLeft(index, item, game.hourCount);
+        let right = moveRight(index, item, game.hourCount);
+        if (left === right) {
+            graph[index] = [left];
+        } else {
+            graph[index] = [left, right];
+        }
+    });
+
+
+}
 
 function dfs(graph, start, visit){
 
@@ -190,5 +260,3 @@ function dfs(graph, start, visit){
 
     //return visited;
 }
-
-//dfs(graph, 2, []);
--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 console.log("index.js loading...")
+
+
+function startNewGame(){
+    console.log("NEW GAME STARTING");
+    let val = document.getElementById("gameSizeId");
+    console.log(val.value);
+
+
+
+    let paras = document.getElementsByClassName("hourSpot");
+
+    while(paras[0]) {paras[0].parentNode.removeChild(paras[0]);}
+
+    newGame(parseInt(val.value));
+    dfsSetUp();
+}
